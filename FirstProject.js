a790fa0fce6ac75be5715b606ffda2380fad8c77
manesr9kk9 <manesr9009@gmail.com>
2020-11-08 22:01:05 +0530

    fs commented

--- FirstProject.js
+++ FirstProject.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const url=require("url");
-const fs=require("fs");
+//const fs=require("fs");
 
  http.createServer(function (req, res) {
    res.writeHead(200, {'Content-Type': 'text/html'});
